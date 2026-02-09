6dd6a2baab1f421452c4b14e228a7b752af333c2
MarcCasadevall <apple5.92@hotmail.com>
2026-02-09 13:54:46 +0100

    end ex2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,9 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> names = Arrays.asList("Pedro","Pablo","Maria","Juan","Marc","Concha");
+        List<String> names = Arrays.asList("Pedro","Pablo","Maria","Juan","Marc","Concha","Pol");
         List<String> result = names.stream()
-                .filter(s -> s.contains("o"))//Lambda Expresion
+                .filter(s -> s.contains("o") && s.length() >= 5)//Lambda Expresion
                 .toList();
 
         System.out.println(result);
