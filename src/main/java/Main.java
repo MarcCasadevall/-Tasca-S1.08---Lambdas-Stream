f69dddb187630c4f94a7d85046a8967c864ab739
MarcCasadevall <apple5.92@hotmail.com>
2026-02-10 11:23:21 +0100

    end ex6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,15 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
         List<String> names = Arrays.asList
                 ("Pedro", "Pablo", "Maria", "Juan", "Marc", "Concha", "Pol");
         List<String> result = names.stream()
                 .filter(s -> s.contains("o") && s.length() >= 5)//Lambda Expresion
                 .toList();
-
         System.out.println(result);
 
         List<String> months = Arrays.asList
@@ -17,6 +18,15 @@ public class Main {
 
         PiValue piValue = () -> 3.1415;
         System.out.println(piValue.getPiValue());
+
+
+        List<Object> elements = Arrays.asList
+                ("Pedro",7, "Pablo",24, "Maria", 10, "Juan", 30,"Marc", "Concha", "Pol");
+        elements.stream()
+                .filter(element -> element instanceof  String)
+                .map(element -> (String) element)
+                .sorted(Comparator.comparingInt(String::length))
+                .forEach(System.out::println);
     }
 
     @FunctionalInterface
