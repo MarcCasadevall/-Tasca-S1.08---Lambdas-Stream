608051e7d8e364ff5534ef2007e4bb0ce60cb538
MarcCasadevall <apple5.92@hotmail.com>
2026-02-10 10:29:50 +0100

    end ex3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,17 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> names = Arrays.asList("Pedro","Pablo","Maria","Juan","Marc","Concha","Pol");
+        List<String> names = Arrays.asList
+                ("Pedro", "Pablo", "Maria", "Juan", "Marc", "Concha", "Pol");
         List<String> result = names.stream()
                 .filter(s -> s.contains("o") && s.length() >= 5)//Lambda Expresion
                 .toList();
 
         System.out.println(result);
+
+        List<String> months = Arrays.asList
+                ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+        months.forEach(System.out::println);
     }
 }
 
